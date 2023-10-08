77833a1b499b14eefe6dcf56611aa5db42304db7
ThatGuyFromEurope <niklas62.35@gmail.com>
2023-10-08 12:00:00 +0100

    'DailyUpdate'

--- content/test.py
+++ content/test.py
@@ -3,18 +3,16 @@ while (x == True):
     import os
     import time
     from time import gmtime, strftime
-    print(strftime("%H:%M", gmtime()))
-    Time = strftime("%H:%M", gmtime())
-    print(type(Time))
-    if (Time == "22:43"):
+    # note one hour behind
+    if (strftime("%H:%M", gmtime()) == "11:00"):
         print("It works")
         os.system("git add .")
         os.system("git commit -m'DailyUpdate'")
         os.system("git push origin main")
         time.sleep(60)
 
-    elif (strftime("%H:%M", gmtime()) == "2:40"):
-        import pandas as pd
+    #else: # (strftime("%H:%M", gmtime()) == "2:40"):
+        #import pandas as pd
         # read by default 1st sheet of an excel file
-        dataframe1 = pd.read_excel('book2.xlsx')
-    x = False
+        #dataframe1 = pd.read_excel('C:/Users/nikla/OneDrive/Desktop/PHW/PWS/PWS/data/phwMaindt.xlsx')
+        #print(dataframe1)
